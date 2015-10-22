dcc5851de505f612a1285bdd2158dd8549fa0708
zhouxiaohui0327 <475503798@qq.com>
2015-10-22 17:05:34 +0800

    success

---
+++ success.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 2015/10/22
+ * Time: 16:34
+ */
+?>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+    <meta content="telephone=no, address=no" name="format-detection">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <link rel="stylesheet" type="text/css" href="./css/base.css"/>
+    <link rel="stylesheet" href="./css/success.css"/>
+    <title>免费吃大餐 点赞筹到钱</title>
+</head>
+<body>
+<div class="content-box">
+        <div class="success_pic">
+            <img src="./images/success.png" alt=""/>
+        </div>
+    <div class="success_info-wrap">
+            <p>姓名</p>
+            <p>手机号</p>
+    </div>
+</div>
+</body>
+</html>
